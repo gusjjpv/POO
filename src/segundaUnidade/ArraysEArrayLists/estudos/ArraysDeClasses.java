40d45d38afbf7b6011ecd96a8951eb97899cccc0
gusDev <joaogustavolima084@gmail.com>
2025-05-29 20:26:23 -0300

    arraysDeClasses

--- src/segundaUnidade/ArraysEArrayLists/estudos/ArraysDeClasses.java
+++ src/segundaUnidade/ArraysEArrayLists/estudos/ArraysDeClasses.java
@@ -1,10 +1,28 @@
 package segundaUnidade.ArraysEArrayLists.estudos;
-
+import java.util.Scanner;
 public class ArraysDeClasses {
   public static void main(String[] args){
     Funcionario funcionarios[] = new Funcionario[20];
-    funcionarios[0] = new Funcionario(01, "Thyago", "Front end");
-    System.out.println(funcionarios[0]);
+    Scanner input = new Scanner(System.in);
+    int opc;
+    int i = 0;
+    do{
+      System.out.println("Informe o nome:");
+      String nome = input.nextLine();
+      System.out.println("Informe a funcao:");
+      String funcao = input.nextLine();
+      funcionarios[i] = new Funcionario(i+1, nome, funcao);
+
+      System.out.println("Quer cadastrar outro funcionario?\n 1-sim 0-nao");
+      opc = input.nextInt();
+      input.nextLine();
+      if(opc == 1){
+        i++;
+      }
+    }while(opc != 0);
     
+    System.out.println(funcionarios[0]);
+    Funcionario.imprimeFuncionarios(funcionarios);
+    input.close();
   }  
 }
--- src/segundaUnidade/ArraysEArrayLists/estudos/Funcionario.java
+++ src/segundaUnidade/ArraysEArrayLists/estudos/Funcionario.java
@@ -1,14 +1,20 @@
 package segundaUnidade.ArraysEArrayLists.estudos;
 
 public class Funcionario {
-    int id;
-    String nome;
-    String funcao;
+    private int id;
+    private String nome;
+    private String funcao;
 
     public Funcionario(int d, String nome, String f){
         this.id = d;
         this.nome = nome;
         this.funcao = f;
+    }
+    //passando um arrays como parametro: tipo nome[]
+    public static void imprimeFuncionarios(Funcionario array[]){
+        for(int i = 1; i < array.length; i++){
+            System.out.println(array[i]);
+        }
     }
 
     public String toString(){
